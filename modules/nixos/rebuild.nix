49e3d36fb36b0606901e1241c5da0720188aaf4c
André Lima <me@limwa.pt>
2025-01-04 19:37:45 +0000

    rebuild(AAAAAAAA): generation 115, NixOS 25.05.20250102.6df2492 with Linux Kernel 6.12.8

--- modules/nixos/rebuild.nix
+++ modules/nixos/rebuild.nix
@@ -20,16 +20,30 @@
           # Go to the config directory
           pushd ~/dotfiles/
 
-          # Only rebuild if there are staged changes
-          if git diff --staged --quiet; then
-            echo "No staged changes detected, exiting."
+          # Sync local repository with remote changes
+          git stash push -qu
+          trap "git stash pop -q --index" EXIT
+          git pull -q --no-autostash
+          trap - EXIT
+          git stash pop -q --index
+
+          # Get the name of the host
+          HOST=$(hostname)
+
+          # Get commit that was last built and current commit
+          last_built_commit=$(git rev-parse --verify --quiet "$HOST^{}" || echo "<not-found>")
+          current_commit=$(git rev-parse HEAD)
+
+          # Only rebuild if there are new commits OR there are staged changes
+          if [ "$last_built_commit" == "$current_commit" ] && git diff --staged --quiet; then
+            echo "No new changes detected, exiting."
             popd
             exit 0
           fi
 
           # Remove any changes that were not added to the index
-          git stash push -k -u
-          trap "git stash pop" EXIT
+          git stash push -qku
+          trap "git stash pop -q" EXIT
 
           # Format files with `nix fmt` and report errors, if any
           nix fmt . &> /dev/null || nix fmt .
@@ -42,10 +56,14 @@
           sudo nixos-rebuild switch --flake .#
 
           # Generate commit message
-          message=$(nixos-rebuild list-generations --json | yq -p=json ".[] | select(.current == true) | \"rebuild($(hostname)): generation \(.generation), NixOS \(.nixosVersion) with Linux Kernel \(.kernelVersion)\"")
+          message=$(nixos-rebuild list-generations --json | yq -p=json ".[] | select(.current == true) | \"rebuild($HOST): generation \(.generation), NixOS \(.nixosVersion) with Linux Kernel \(.kernelVersion)\"")
 
           # Commit all changes witih the generation metadata
-          git commit -m "$message"
+          git commit -m "$message" --allow-empty
+          git tag -af -m "$HOST" "$HOST"
+
+          # Update remote
+          git push -q --tags
 
           # Notify all OK!
           notify-send -e "NixOS succesfuly rebuilt!" --icon=software-update-available
