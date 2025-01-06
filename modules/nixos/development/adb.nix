4afe8a9eb5a1b762fc53dfeee6abe7a687389aa1
André Lima <me@limwa.pt>
2025-01-06 15:43:54 +0000

    rebuild(AAAAAAAA): generation 129, NixOS 25.05.20250104.8f3e1f8 with Linux Kernel 6.12.8

--- modules/nixos/development/adb.nix
+++ modules/nixos/development/adb.nix
@@ -5,4 +5,8 @@
 }: {
   services.udev.packages = [pkgs.android-udev-rules];
   users.users.${user.login}.extraGroups = ["adbusers" "plugdev"];
+
+  # Create groups if they don't exist yet
+  users.groups.adbusers = {};
+  users.groups.plugdev = {};
 }
