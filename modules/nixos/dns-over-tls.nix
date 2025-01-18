2c6db7233f4baaebc61864a6519f1784d155cb54
André Lima <me@limwa.pt>
2025-01-18 18:21:15 +0000

    rebuild(AAAAAAAA): generation 145, NixOS 25.05.20250116.5df4362 with Linux Kernel 6.12.9

---
+++ modules/nixos/dns-over-tls.nix
@@ -0,0 +1,15 @@
+{
+  # systemd-resolved
+  # https://nixos.wiki/wiki/Systemd-resolved
+
+  networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+
+  services.resolved = {
+    enable = true;
+
+    fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+
+    dnssec = "true";
+    dnsovertls = "opportunistic";
+  };
+}
