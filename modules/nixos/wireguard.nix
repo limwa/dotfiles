7b6d40c89b4494705065754e2677ddcef2ac4b80
André Lima <me@limwa.pt>
2025-01-20 16:24:06 +0000

    rebuild(AAAAAAAA): generation 160, NixOS 25.05.20250116.5df4362 with Linux Kernel 6.12.9

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -7,20 +7,29 @@
   # https://wiki.nixos.org/wiki/WireGuard#Setting_up_WireGuard_with_NetworkManager
 
   environment.systemPackages = [pkgs.reaction];
-  networking.firewall = {
-    checkReversePath = "loose";
 
-    # if packets are still dropped, they will show up in dmesg
-    logReversePathDrops = true;
+  systemd.network = {
+    enable = true;
+    wait-online.enable = false;
+  };
 
-    extraCommands = ''
-      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport ${wireguard.port} -j RETURN
-      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport ${wireguard.port} -j RETURN
-    '';
+  networking = {
+    useNetworkd = true;
 
-    extraStopCommands = ''
-      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport ${wireguard.port} -j RETURN || true
-      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport ${wireguard.port} -j RETURN || true
-    '';
+    firewall = {
+      checkReversePath = "loose";
+      # if packets are still dropped, they will show up in dmesg
+      logReversePathDrops = true;
+
+      extraCommands = ''
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport ${wireguard.port} -j RETURN
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport ${wireguard.port} -j RETURN
+      '';
+
+      extraStopCommands = ''
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport ${wireguard.port} -j RETURN || true
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport ${wireguard.port} -j RETURN || true
+      '';
+    };
   };
 }
