d8c101a510e7ce76d75053a3e02f024436cb9a46
André Lima <me@limwa.pt>
2025-01-25 20:47:24 +0000

    rebuild(AAAAAAAA): generation 171, NixOS 25.05.20250123.0aa4755 with Linux Kernel 6.12.10

--- modules/nixos/hardware/graphics/nvidia.nix
+++ modules/nixos/hardware/graphics/nvidia.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   # Nvidia
   # https://nixos.wiki/wiki/Nvidia
 
@@ -8,4 +12,35 @@
 
   # Use the beta NVIDIA drivers. Stable drivers are available too.
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+  # Create specializations for Sync and Reverse Sync modes.
+  specialisation = lib.mkIf (config.hardware.nvidia.prime.offload.enable) {
+    with-sync.configuration = {
+      hardware.nvidia = {
+        prime = {
+          offload.enable = false;
+          sync.enable = true;
+        };
+
+        powerManagement.finegrained = false;
+      };
+    };
+
+    with-reverse-sync.configuration = {
+      hardware.nvidia = {
+        prime = {
+          offload.enable = false;
+          reverseSync.enable = true;
+        };
+
+        powerManagement.finegrained = false;
+      };
+    };
+
+    with-offload-and-reverse-sync.configuration = {
+      hardware.nvidia.prime = {
+        reverseSync.enable = true;
+      };
+    };
+  };
 }
