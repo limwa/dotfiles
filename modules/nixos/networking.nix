df2bc614047d4a658c39c892e48d004b945fb0d8
André Lima <me@limwa.pt>
2025-01-26 15:01:28 +0000

    rebuild(AAAAAAAA): generation 172, NixOS 25.05.20250123.0aa4755 with Linux Kernel 6.12.10

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -4,6 +4,7 @@
   users.users.${user.login}.extraGroups = ["networkmanager"];
 
   # Fix for L2TP VPN crashing when establishing connection.
+  networking.networkmanager.enableStrongSwan = true;
   services.strongswan = {
     enable = true;
     secrets = [
