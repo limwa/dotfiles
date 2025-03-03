1db566897ce957ca38a9f0499e1576e0d6879dcd
André Lima <me@limwa.pt>
2025-03-03 16:29:22 +0000

    rebuild(AAAAAAAA): generation 202, NixOS 25.05.20250227.6313551 with Linux Kernel 6.13.4

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -22,13 +22,13 @@
       logReversePathDrops = true;
 
       extraCommands = ''
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport ${wireguard.port} -j RETURN
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport ${wireguard.port} -j RETURN
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
       '';
 
       extraStopCommands = ''
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport ${wireguard.port} -j RETURN || true
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport ${wireguard.port} -j RETURN || true
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
       '';
     };
   };
