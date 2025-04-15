250663bcad564f1c52a045dd9aaf456b6e87e1b4
André Lima <me@limwa.pt>
2025-04-15 19:47:29 +0100

    rebuild(AAAAAAAA): generation 233, NixOS 25.05.20250412.2631b0b with Linux Kernel 6.14.2

--- packages/warp-terminal/default.nix
+++ packages/warp-terminal/default.nix
@@ -8,7 +8,7 @@ appimageTools.wrapType2 rec {
   version = "v0.2025.04.09.08.11.stable_02";
 
   src = fetchurl {
-    url = "https://releases.warp.dev/stable/v0.2025.04.09.08.11.stable_02/Warp-x86_64.AppImage";
+    url = "https://releases.warp.dev/stable/${version}/Warp-x86_64.AppImage";
     sha256 = "sha256-h5JiMfxyk7V54Gwy99oeOG9ClVWWbY/eb5oP/0ml86Y=";
   };
 
