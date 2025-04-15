cd20400c5399c5895a7184fe5f85f3229147318f
André Lima <me@limwa.pt>
2025-04-15 16:18:56 +0100

    rebuild(AAAAAAAA): generation 222, NixOS 25.05.20250412.2631b0b with Linux Kernel 6.14.2

--- packages/tableplus/default.nix
+++ packages/tableplus/default.nix
@@ -1,96 +1,24 @@
 {
   stdenv,
   fetchurl,
+  appimageTools,
   lib,
-  wrapGAppsHook3,
-  cpio,
-  rpm,
-  cairo,
-  gdk-pixbuf,
-  glib,
-  gtk3,
-  gtksourceview,
-  json-glib,
-  krb5,
-  libgee,
-  libsecret,
-  pango,
-  sqlite,
 }: let
-  version = "1.2.4-260";
-
-  rpath =
-    lib.makeLibraryPath [
-      cairo
-      gdk-pixbuf
-      glib
-      gtk3
-      gtksourceview
-      json-glib
-      krb5.lib
-      libgee
-      libsecret
-      pango
-      sqlite
-    ]
-    + ":${stdenv.cc.cc.lib}/lib";
-
-  src =
+  url =
     if stdenv.hostPlatform.system == "x86_64-linux"
-    then
-      fetchurl {
-        url = "https://yum.tableplus.com/rpm/x86_64/tableplus-${version}.x86_64.rpm";
-        sha256 = "sha256-jBT4l85kE1hOYnfnXHVrsIq5NfaX6cq7Af8WzanCOvI=";
-      }
+    then "https://tableplus.com/release/linux/x64/TablePlus-x64.AppImage"
+    else if stdenv.hostPlatform.system == "aarch64-linux"
+    then "https://tableplus.com/release/linux/arm64/TablePlus-aarch64.AppImage"
     else throw "This derivation for TablePlus is not supported on ${stdenv.hostPlatform.system}";
 in
-  stdenv.mkDerivation {
+  appimageTools.wrapType2 {
     pname = "tableplus";
-    inherit version;
+    version = "1.2.6-260";
 
-    system = "x86_64-linux";
-
-    inherit src;
-
-    nativeBuildInputs = [
-      wrapGAppsHook3
-      glib # For setup hook populating GSETTINGS_SCHEMA_PATH
-    ];
-
-    buildInputs = [
-      cpio
-      rpm
-    ];
-
-    unpackPhase = ''
-      mkdir -p $out
-      rpm2cpio $src | cpio -idmv -D $out
-    '';
-
-    installPhase = ''
-      mkdir -p $out/bin $out/share/tableplus $out/share/applications
-      mv -v $out/opt/tableplus/tableplus.desktop $out/share/applications
-      mv -v $out/opt/tableplus $out/share
-
-      ln -sv "$out/share/tableplus/tableplus" "$out/bin/tableplus"
-
-      rm -rf $out/opt $out/usr
-
-      # Otherwise it looks "suspicious"
-      chmod -R g-w $out
-    '';
-
-    postFixup = ''
-      for file in $(find $out -type f \( -perm /0111 -o -name \*.so\* -or -name \*.node\* \) ); do
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
-        patchelf --set-rpath ${rpath}:$out/share/tableplus $file || true
-      done
-
-      # Fix the desktop link
-      substituteInPlace $out/share/applications/tableplus.desktop \
-        --replace-fail /usr/local/bin/ $out/bin/ \
-        --replace-fail /opt/tableplus/ $out/share/tableplus/
-    '';
+    src = fetchurl {
+      inherit url;
+      sha256 = "sha256-4HIPkWqpIcyycpqs3ELcQZUlgmcXeHxdsJ6gS8YmIAg=";
+    };
 
     meta = {
       description = "Database management made easy";
@@ -98,4 +26,19 @@ in
       license = lib.licenses.unfree;
       platforms = ["x86_64-linux"];
     };
+
+    extraPkgs = pkgs:
+      with pkgs; [
+        e2fsprogs
+        fontconfig.lib
+        freetype
+        fribidi
+        harfbuzz
+        kdePackages.wayland
+        libgcc.lib
+        libgpg-error
+        libz
+        xorg.libX11
+        xorg.libxcb
+      ];
   }
