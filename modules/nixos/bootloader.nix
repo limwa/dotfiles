e075e87dc4ff689d441227c365087481bcdba3cf
André Lima <me@limwa.pt>
2025-04-22 00:38:31 +0100

    rebuild(AAAAAAAA): generation 241, NixOS 25.05.20250417.b024ced with Linux Kernel 6.14.2

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -1,6 +1,9 @@
 {
   # The systemd-boot bootloader is used as the default bootloader.
   # However, it is disabled if the secure boot module is imported.
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 3;
+  };
 }
