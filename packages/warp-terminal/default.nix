4af0b94512fedbfa15e9525f9c60a5949e3cfdfa
André Lima <me@limwa.pt>
2025-04-26 17:57:23 +0100

    rebuild(AAAAAAAA): generation 245, NixOS 25.05.20250424.f771eb4 with Linux Kernel 6.14.3

--- packages/warp-terminal/default.nix
+++ packages/warp-terminal/default.nix
@@ -3,10 +3,10 @@
   old-warp-terminal,
 }:
 old-warp-terminal.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "0.2025.04.16.08.11.stable_02";
+  version = "0.2025.04.23.08.11.stable_01";
 
   src = fetchurl {
     inherit (prevAttrs.src) url;
-    sha256 = "sha256-+/0PA8Z/IbgHNtk78v4d3cw0G8Kk1MQ+gN16/8nKmGg=";
+    sha256 = "sha256-RwKWCU3bldMlS4M+tmfxGE/1d+lMxMPbjdbzpptkmv4=";
   };
 })
