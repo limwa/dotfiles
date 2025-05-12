0375a7b2db490d145295daf7cee8b1c1b8b606a5
André Lima <me@limwa.pt>
2025-05-12 16:49:34 +0100

    rebuild(AAAAAAAA): generation 260, NixOS 25.05.20250512.5567436 with Linux Kernel 6.14.6

--- hosts/AAAAAAAA/modules/hardware.nix
+++ hosts/AAAAAAAA/modules/hardware.nix
@@ -17,12 +17,9 @@ in {
 
   services.xserver.videoDrivers = ["nvidia"];
 
-  boot = {
-    kernelModules = ["legion-laptop"];
-    extraModulePackages = with config.boot.kernelPackages; [
-      lenovo-legion-module
-    ];
-  };
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    lenovo-legion-module
+  ];
 
   hardware.nvidia = {
     dynamicBoost.enable = lib.mkDefault true;
