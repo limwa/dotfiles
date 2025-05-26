5134082980ced0e6109fb8b7414dfe7a1f2f2485
André Lima <me@limwa.pt>
2025-05-26 18:43:59 +0100

    rebuild(AAAAAAAA): generation 275, NixOS 25.11.20250523.063f43f with Linux Kernel 6.14.8

--- flake.nix
+++ flake.nix
@@ -200,7 +200,6 @@
           disko.nixosModules.disko
           ./modules/nixos/audio
           ./modules/nixos/audio/soundwire.nix
-          ./modules/nixos/desktop/cosmic
           ./modules/nixos/desktop/gnome
           ./modules/nixos/development/adb.nix
           ./modules/nixos/development/ccache.nix
