a279bd168407ed5fdf198d10149609d646a2e59f
André Lima <me@limwa.pt>
2025-06-03 14:15:34 +0100

    rebuild(AAAAAAAA): generation 286, NixOS 25.11.20250531.910796c with Linux Kernel 6.12.31

--- packages/cursor/default.nix
+++ packages/cursor/default.nix
@@ -1,15 +1,26 @@
-{code-cursor}:
-code-cursor.override (old: {
-  appimageTools =
-    old.appimageTools
-    // {
-      wrapType2 = {extraPkgs ? pkgs: [], ...} @ args:
-        old.appimageTools.wrapType2 (args
-          // {
-            extraPkgs = pkgs: let
-              openssh = pkgs.callPackage ./openssh {};
-            in
-              extraPkgs pkgs ++ [openssh];
-          });
-    };
-})
+{
+  lib,
+  newScope,
+  openssh,
+  appimageTools,
+  code-cursor,
+}: let
+  scope = lib.makeScope newScope (overrides: {
+    openssh = overrides.callPackage ./openssh {inherit openssh;};
+
+    appimageTools =
+      appimageTools
+      // {
+        wrapType2 = {extraPkgs ? pkgs: [], ...} @ args:
+          appimageTools.wrapType2 (
+            args
+            // {
+              extraPkgs = pkgs: extraPkgs pkgs ++ [overrides.openssh];
+            }
+          );
+      };
+
+    code-cursor = overrides.callPackage code-cursor.override {};
+  });
+in
+  scope.code-cursor
--- packages/cursor/openssh/default.nix
+++ packages/cursor/openssh/default.nix
@@ -1,4 +1,8 @@
 {openssh}:
 openssh.overrideAttrs (prev: {
   patches = (prev.patches or []) ++ [./openssh-nocheckcfg.patch];
+
+  postInstall = ''
+    echo "hey" > $out/lmao
+  '';
 })
