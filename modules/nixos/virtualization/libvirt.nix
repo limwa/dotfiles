8007f711f033a958574173ab9d952e1600250461
André Lima <me@limwa.pt>
2025-06-08 16:09:56 +0100

    rebuild(AAAAAAAA): generation 292, NixOS 25.11.20250607.6dbd508 with Linux Kernel 6.15.1

--- modules/nixos/virtualization/libvirt.nix
+++ modules/nixos/virtualization/libvirt.nix
@@ -1,4 +1,8 @@
-{user, ...}: {
+{
+  user,
+  pkgs,
+  ...
+}: {
   # Libvirt and Virt-manager
   # https://nixos.wiki/wiki/Virt-manager
 
@@ -6,7 +10,18 @@
   programs.virt-manager.enable = true;
 
   # Enable libvirtd.
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_full;
+
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [pkgs.OVMFFull.fd];
+      };
+    };
+  };
 
   # Add libvirtd group to the user.
   users.users.${user.login}.extraGroups = ["libvirtd"];
--- modules/nixos/virtualization/virtualbox.nix
+++ modules/nixos/virtualization/virtualbox.nix
@@ -1,9 +1,19 @@
-{user, ...}: {
+{
+  user,
+  config,
+  ...
+}: {
   # VirtualBox
   # https://nixos.wiki/wiki/VirtualBox
 
   # Enable VirtualBox.
-  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host = let
+    useKvm = config.virtualisation.libvirtd.enable;
+  in {
+    enable = true;
+    enableKvm = useKvm;
+    addNetworkInterface = !useKvm;
+  };
 
   # Add vboxusers group to the user.
   users.users.${user.login}.extraGroups = ["vboxusers"];
