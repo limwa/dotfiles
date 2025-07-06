d64e4a8e243b55788b50fe68b97b7a100e0adbd0
André Lima <me@limwa.pt>
2025-07-06 12:38:35 +0100

    fix: move strongswan to NM plugins

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,10 +1,22 @@
-{user, ...}: {
+{user, pkgs, ...}: {
   # Enable networking.
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [ 
+      networkmanager-fortisslvpn
+      networkmanager-iodine
+      networkmanager-l2tp
+      networkmanager-openconnect
+      networkmanager-openvpn
+      networkmanager-sstp
+      networkmanager-strongswan
+      networkmanager-vpnc
+    ];
+  };
+
   users.users.${user.login}.extraGroups = ["networkmanager"];
 
   # Fix for L2TP VPN crashing when establishing connection.
-  networking.networkmanager.enableStrongSwan = true;
   services.strongswan.enable = true;
   environment.etc."strongswan.conf".text = "";
 
