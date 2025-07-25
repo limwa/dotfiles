05dbc97331af89ece7115c81758c099a41bf5033
André Lima <me@limwa.pt>
2025-07-25 18:37:24 +0100

    rebuild(AAAAAAAA): generation 331, NixOS 25.11.20250708.9807714 with Linux Kernel 6.15.5

--- modules/debug.nix
+++ modules/debug.nix
@@ -1,1 +1,45 @@
-{}
+{pkgs, ...}: {
+  # https://unix.stackexchange.com/questions/739363/a-systemd-service-that-runs-just-before-shutdown-and-uses-a-mounted-filesystem
+  systemd.services."dump-shutdown-processes" = {
+    unitConfig = {
+      Description = "Dump shutdown processes";
+      RequiresMountsFor = ["/home"];
+    };
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.coreutils}/bin/true";
+      ExecStop = let
+        script = pkgs.writeShellApplication {
+          name = "dump-shutdown-processes";
+
+          runtimeInputs = [
+            pkgs.procps
+            pkgs.coreutils
+          ];
+
+          text = ''
+            current_time="$(date +%Y-%m-%dT%H:%M:%S)"
+            dump_file="/home/lima/shutdown-processes-$current_time.log"
+
+            # Dump all processes
+            for pid in $(pgrep \.); do
+              printf '> PID=%s EXE=%s CWD=%s CMD=%s\n' \
+                "$pid" \
+                "$(realpath "/proc/$pid/exe")" \
+                "$(realpath "/proc/$pid/cwd")" \
+                "$(cat "/proc/$pid/cmdline")" >> "$dump_file"
+            done
+
+            echo "Done!" >> "$dump_file"
+          '';
+        };
+      in "${script}/bin/${script.name}";
+
+      TimeoutSec = "infinity";
+    };
+
+    wantedBy = ["multi-user.target"];
+  };
+}
