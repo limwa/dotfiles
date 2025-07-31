798fe384d760db652bda4045ed2cf67fcac68216
André Lima <me@limwa.pt>
2025-07-31 19:50:14 +0100

    rebuild(AAAAAAAA): generation 337, NixOS 25.11.20250731.72eb4af with Linux Kernel 6.15.5

--- modules/nixos/virtualization/libvirt.nix
+++ modules/nixos/virtualization/libvirt.nix
@@ -14,12 +14,7 @@
     enable = true;
     qemu = {
       package = pkgs.qemu_full;
-
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [pkgs.OVMFFull.fd];
-      };
     };
   };
 
