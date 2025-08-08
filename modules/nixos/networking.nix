67031fffd9dea02e0da66fcc44c907302deb154e
André Lima <me@limwa.pt>
2025-08-08 19:32:04 +0100

    rebuild(AAAAAAAA): generation 370, NixOS 25.11.20250805.355cef4 with Linux Kernel 6.16.0

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -30,5 +30,10 @@
   specialisation."with-nftables".configuration = {
     # Use nftables as the default firewall backend
     networking.nftables.enable = true;
+
+    networking.firewall.interfaces."virbr0" = {
+      allowedTCPPorts = [53];
+      allowedUDPPorts = [53 67];
+    };
   };
 }
