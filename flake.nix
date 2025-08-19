c8a09cc4f5269a862d4005e849eb76727575aa77
André Lima <me@limwa.pt>
2025-08-19 20:27:48 +0100

    rebuild(AAAAAAAA): generation 382, NixOS 25.11.20250708.9807714 with Linux Kernel 6.15.5

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs (NixOS unstable)
-    nixpkgs.url = "github:limwa/nixpkgs/dotfiles";
+    nixpkgs.url = "github:limwa/nixpkgs/9807714d6944a957c2e036f84b0ff8caf9930bc0";
 
     # Agenix
     agenix.url = "github:ryantm/agenix";
