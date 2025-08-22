ba9b44bd10a56687a3017e2c5c068df507e40cdd
André Lima <me@limwa.pt>
2025-08-22 18:33:31 +0100

    rebuild(AAAAAAAA): generation 393, NixOS 25.11.20250820.cf03a59 with Linux Kernel 6.16.1

--- modules/nixos/desktop/apps.nix
+++ modules/nixos/desktop/apps.nix
@@ -6,8 +6,12 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # Accept Android SDK licenses
+  nixpkgs.config.android_sdk.accept_license = true;
+
   # User-scoped apps
   users.users.${user.login}.packages = with pkgs; [
+    android-studio-full
     code-cursor
     discord
     eclipses.eclipse-modeling
