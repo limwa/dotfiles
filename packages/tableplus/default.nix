8fa98ecc12952bfe358c82d1a0c550ea58037913
André Lima <me@limwa.pt>
2025-09-13 19:00:20 +0100

    rebuild(AAAAAAAA): generation 406, NixOS 25.11.20250913.c6ba936 with Linux Kernel 6.16.5

--- packages/tableplus/default.nix
+++ packages/tableplus/default.nix
@@ -4,37 +4,32 @@
   appimageTools,
   lib,
 }:
-appimageTools.wrapType2 rec {
-  pname = "tableplus";
-  version = "1.2.6-264";
+assert stdenv.hostPlatform.system == "x86_64-linux";
+  appimageTools.wrapType2 rec {
+    pname = "tableplus";
+    version = "1.2.8-266";
 
-  src = fetchurl {
-    url =
-      if stdenv.hostPlatform.system == "x86_64-linux"
-      then "https://tableplus.com/release/linux/x64/TablePlus-x64.AppImage"
-      else if stdenv.hostPlatform.system == "aarch64-linux"
-      then "https://tableplus.com/release/linux/arm64/TablePlus-aarch64.AppImage"
-      else throw "This derivation for TablePlus is not supported on ${stdenv.hostPlatform.system}";
-
-    hash = "sha256-4HIPkWqpIcyycpqs3ELcQZUlgmcXeHxdsJ6gS8YmIAg=";
-  };
-
-  extraInstallCommands = let
-    appimageContents = appimageTools.extract {
-      inherit pname version src;
+    src = fetchurl {
+      url = "https://files.tableplus.com/linux/x64/266/TablePlus-x64.AppImage";
+      hash = "sha256-3qMFdjtwnGyGTZqHgKRA9RKH4cQgOJ9RsS/0hzH+tKU=";
     };
-  in ''
-    install -D -m444 ${appimageContents}/usr/share/applications/tableplus-appimage.desktop $out/share/applications/tableplus.desktop
-    install -D -m444 ${appimageContents}/usr/share/icons/hicolor/256x256/apps/tableplus.png $out/share/icons/hicolor/256x256/apps/tableplus.png
-  '';
 
-  meta = {
-    description = "Database management made easy";
-    homepage = "https://tableplus.com/";
-    license = lib.licenses.unfree;
-    platforms = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-  };
-}
+    extraInstallCommands = let
+      appimageContents = appimageTools.extract {
+        inherit pname version src;
+      };
+    in ''
+      install -D -m444 ${appimageContents}/usr/share/applications/tableplus-appimage.desktop $out/share/applications/tableplus.desktop
+      install -D -m444 ${appimageContents}/usr/share/icons/hicolor/256x256/apps/tableplus.png $out/share/icons/hicolor/256x256/apps/tableplus.png
+    '';
+
+    meta = {
+      description = "Database management made easy";
+      homepage = "https://tableplus.com/";
+      license = lib.licenses.unfree;
+      platforms = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    };
+  }
