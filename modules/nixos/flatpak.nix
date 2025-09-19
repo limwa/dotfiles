1bd786b982ee3f1a1a937d96bc774dfa0364a680
André Lima <me@limwa.pt>
2025-09-19 12:14:23 +0100

    rebuild(AAAAAAAA): generation 412, NixOS 25.11.20250919.3cdb130 with Linux Kernel 6.16.7

--- modules/nixos/flatpak.nix
+++ modules/nixos/flatpak.nix
@@ -11,6 +11,9 @@
   # Automatically configure the Flathub remote.
   systemd.services.flatpak-repo = {
     wantedBy = ["multi-user.target"];
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+
     path = [pkgs.flatpak];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
