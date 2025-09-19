ada84ddc89bdb1e1d75ad37af66ac550de5cc15f
André Lima <me@limwa.pt>
2025-09-19 12:08:01 +0100

    rebuild(AAAAAAAA): generation 411, NixOS 25.11.20250919.3cdb130 with Linux Kernel 6.16.7

--- modules/nixos/virtualization/virtualbox.nix
+++ modules/nixos/virtualization/virtualbox.nix
@@ -1,19 +1,10 @@
-{
-  user,
-  config,
-  ...
-}: {
+{user, ...}: {
   # VirtualBox
   # https://nixos.wiki/wiki/VirtualBox
 
   # Enable VirtualBox.
-  virtualisation.virtualbox.host = let
-    useKvm = config.virtualisation.libvirtd.enable;
-  in {
-    enable = true;
-    enableKvm = useKvm;
-    addNetworkInterface = !useKvm;
-  };
+  virtualisation.virtualbox.host.enable = true;
+  boot.kernelParams = ["kvm.enable_virt_at_load=0"];
 
   # Add vboxusers group to the user.
   users.users.${user.login}.extraGroups = ["vboxusers"];
