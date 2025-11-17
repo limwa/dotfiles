423a1a092a99bfdddb9db4195561241fea1579eb
André Lima <me@limwa.pt>
2025-11-17 21:45:14 +0000

    rebuild(AAAAAAAA): generation 442, NixOS 25.11.20251117.518e621 with Linux Kernel 6.17.8

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -3,7 +3,6 @@
     (final: prev: {
       custom = {
         fractal = final.callPackage ./fractal {};
-        gnomeExtensions.media-controls = final.callPackage ./gnomeExtensions/media-controls {};
         tableplus = final.callPackage ./tableplus {};
       };
     })
