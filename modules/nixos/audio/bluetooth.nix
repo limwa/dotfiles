cdef006b9abe99e08e61aec6c007133ec3b6c592
André Lima <me@limwa.pt>
2025-12-30 16:43:27 +0000

    rebuild(AAAAAAAA): generation 511, NixOS 26.05.20251228.c0b0e0f with Linux Kernel 6.18.2

--- modules/nixos/audio/bluetooth.nix
+++ modules/nixos/audio/bluetooth.nix
@@ -3,21 +3,7 @@
   # https://nixos.wiki/wiki/Bluetooth
   imports = [./.];
 
-  hardware.bluetooth = {
-    enable = true;
-
-    settings = {
-      General = {
-        # https://askubuntu.com/questions/676853/bluetooth-headset-with-poor-sound-quality-on-ubuntu
-        AutoConnect = true;
-        MultiProfile = "multiple";
-
-        # Enable experimental features.
-        Experimental = true;
-        KernelExperimental = true;
-      };
-    };
-  };
+  hardware.bluetooth.enable = true;
 
   # Allow audio and media keys to be used with bluetooth.
   home-manager.users.${user.login} = {
