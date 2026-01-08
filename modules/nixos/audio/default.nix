abb8dde358d6d373863036da8ae8d5a342adc322
André Lima <me@limwa.pt>
2026-01-08 17:07:21 +0000

    rebuild(AAAAAAAA): generation 535, NixOS 26.05.20260106.c122e60 with Linux Kernel 6.18.3

--- modules/nixos/audio/default.nix
+++ modules/nixos/audio/default.nix
@@ -5,11 +5,6 @@
   # Enable sound with pipewire.
   security.rtkit.enable = true;
 
-  boot.extraModprobeConfig = ''
-    options snd_hda_intel model=auto
-    options snd_hda_intel dmic_detect=0"
-  '';
-
   services.pipewire = {
     enable = true;
     audio.enable = true;
@@ -17,26 +12,6 @@
     pulse.enable = true;
     wireplumber = {
       enable = true;
-      extraConfig = {
-        "95-disable-mic-boost" = {
-          "monitor.alsa.rules" = [
-            {
-              matches = [
-                {
-                  "device.name" = "~alsa_card.*";
-                  "device.vendor.id" = "0x1022";
-                  "device.product.id" = "0x15e3";
-                }
-              ];
-              actions = {
-                update-props = {
-                  "api.alsa.soft-mixer" = true;
-                };
-              };
-            }
-          ];
-        };
-      };
     };
   };
 }
