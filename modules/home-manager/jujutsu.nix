463ba32cd9f353457179493fcdcac077ccf9ffaa
André Lima <me@limwa.pt>
2026-03-01 18:08:52 +0000

    rebuild(AAAAAAAA): generation 554, NixOS 26.05.20260212.acdf92d with Linux Kernel 6.12.69

--- modules/home-manager/jujutsu.nix
+++ modules/home-manager/jujutsu.nix
@@ -12,6 +12,12 @@
           name = lib.mkDefault user.displayName;
           email = lib.mkDefault user.email;
         };
+
+        aliases = {
+          tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@"];
+          tug- = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
+          mine = ["bookmark" "list" "-r" "mine()"];
+        };
       };
     };
   };
