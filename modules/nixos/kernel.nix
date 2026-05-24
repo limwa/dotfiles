2696b5706b86d4839dda6a1bde6c5a8742962ddc
André Lima <me@limwa.pt>
2026-05-24 17:34:37 +0100

    rebuild(AAAAAAAA): generation 594, NixOS 26.05.20260523.2991645 with Linux Kernel 7.0.9

--- modules/nixos/kernel.nix
+++ modules/nixos/kernel.nix
@@ -7,4 +7,14 @@
   # Linux Kernel
   # https://nixos.wiki/wiki/Linux_kernel
   boot.kernelPackages = if useLatestKernel then pkgs.linuxPackages_latest else pkgs.linuxPackages;
+
+  boot.kernelPatches = [
+    {
+      name = "btmtk-accept-too-short-wmt-func-ctrl-events";
+      patch = pkgs.fetchpatch {
+        url = "https://github.com/torvalds/linux/commit/e3ac0d9f1a205f33a43fba3b79ef74d2f604c78b.patch?full_index=1";
+        hash = "sha256-DE6im1PmLWFYRk2QtfCWXfBzBCMT4fyUgufDhUn0wL8=";
+      };
+    }
+  ];
 }
